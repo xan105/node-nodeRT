cbe0f4ecd9c4ad8d08816b6bf43cf5916429d5e5
Anthony Beaumont <dev.xan105@outlook.be>
2024-09-07 11:42:11 +0700

    typo

--- scripts/updateCompilerFlags.js
+++ scripts/updateCompilerFlags.js
@@ -22,7 +22,7 @@ for (const scope of scopes)
     const gyp = await readJSON(binding);
     
 /*
-Node 22 is using "/std:c++20" this is an issue because it's incompatible with "/ZW" (C++/CX), this is unlikely to change as Microsoft has abandonned uwp/winrt and recommends to [move to C++/WinRT from C++/CX](https://learn.microsoft.com/en-us/windows/uwp/cpp-and-winrt-apis/move-to-winrt-from-cx).
+Node 22 is using "/std:c++20" this is an issue because it's incompatible with "/ZW" (C++/CX), this is unlikely to change as Microsoft has abandoned uwp/winrt and recommends to [move to C++/WinRT from C++/CX](https://learn.microsoft.com/en-us/windows/uwp/cpp-and-winrt-apis/move-to-winrt-from-cx).
 Hence the forcing of "/std:c++17" via binding.gyp (using CL.exe env var gets overriden 🙄).
 */
     
